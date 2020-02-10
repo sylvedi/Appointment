667d131ce0dae7ca7eef644eea17c80edfcf1cf9
Sylvanus Edi <SEdi@my.gcu.edu>
2020-02-09 17:56:32 -0700

    Add project files.

---
+++ Appointment/Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using Appointment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appointment.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowAppointmentDetails(AppointmentModel appointment)
+        {
+            if (appointment.doctorLastName == null)
+            {
+                appointment.doctorLastName = "CHamBerger";
+            }
+            if (ModelState.IsValid)
+                return View("ShowAppointmentDetails", appointment);
+            else
+                return View("Index");
+        }
+    }
+
+
+}
---
+++ Appointment/Models/AppointmentModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Appointment.Models
+{
+    public class AppointmentModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength =4)]
+        [DisplayName("Patient's full name: ")]
+        public string patientName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Choose a visit date: ")]
+        public DateTime appointmentDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [DisplayName("Networth: ")]
+        public decimal patientNetworth { get; set; }
+
+
+        [DisplayName("Recommended doctor: ")]
+        public string doctorLastName { get; set; } = "CHamBerger";
+
+        [Required]
+        [Range(1, 10)]
+        [DisplayName("pain level (0-10): ")]
+        public int currentPainLevel { get; set; }
+
+        public AppointmentModel(string patientName, DateTime appointmentDate, 
+            decimal patientNetworth, string doctorLastName, int currentPainLevel)
+        {
+            this.patientName = patientName;
+            this.appointmentDate = appointmentDate;
+            this.patientNetworth = patientNetworth;
+            this.doctorLastName = doctorLastName;
+            this.currentPainLevel = currentPainLevel;
+        }
+
+        public AppointmentModel()
+        {
+        }
+    }
+}
